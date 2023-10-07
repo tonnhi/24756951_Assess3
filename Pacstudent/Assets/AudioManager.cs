e094643ad17fb6789ba399a79fd82b179e243c07
tonnhi <tnulam@gmail.com>
2023-10-07 18:36:28 +1100

    Update AudioManager.cs

--- Pacstudent/Assets/AudioManager.cs
+++ Pacstudent/Assets/AudioManager.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public AudioSource introBackgroundMusic;
+    public AudioSource normalStateBackgroundMusic;
+
     void Start()
     {
-        
+        introBackgroundMusic.Play();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (!introBackgroundMusic.isPlaying && !normalStateBackgroundMusic.isPlaying)
+        {
+            // Your logic to determine when the transition should happen
+            /* Your condition for the transition */
+            {
+                introBackgroundMusic.Stop();
+                normalStateBackgroundMusic.Play();
+            }
+        }
     }
 }
