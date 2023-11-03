344f674521dc88ba243e63464eed50c1407bf6c8
tonnhi <tnulam@gmail.com>
2023-11-03 20:55:18 +1100

    adding in ui textfields

--- Pacstudent/Assets/Scripts/Health.cs
+++ Pacstudent/Assets/Scripts/Health.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
+    public int health;
+    public int numOfLives;
+
+    public Image[] Lives;
+    public Sprite Paclife;
+    public Sprite noLife;
         
-    }
 }
